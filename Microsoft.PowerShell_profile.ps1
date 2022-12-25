a14628cbd1d062f33375cea5362c338bea6296db
Masachika Kamada <63488322+masachika-kamada@users.noreply.github.com>
2022-12-26 03:40:40 +0900

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,29 +1,78 @@
+function setting(){
+  notepad $profile
+}
+
+# --- Docker --- #
 function di(){
   docker images
 }
-
 function dp(){
   docker ps
 }
-
 function dpa(){
   docker ps -a
 }
-
+function psa(){
+  docker ps -a
+}
 function dcur(){
   docker container ls -f status=running -l -q
 }
-
 function de(){
   docker exec -it $(dcur) bash
 }
-
 function dstart(){
   docker start $args
 }
-
 function dstop(){
   docker stop $(dcur)
 }
-
 Set-Alias d docker
+
+# --- Git ---#
+function gadd(){
+  git add .
+}
+function gcom(){
+  git commit -m $args
+}
+function grem(){
+  git remote add origin $args
+}
+function gpush(){
+  git push
+}
+function gpushu(){
+  git push -u origin main
+}
+
+# --- General ---#
+function ll(){
+  Get-ChildItem -force
+}
+function tar(){
+  tar -xzf $args
+}
+
+# --- Anaconda ---#
+function conda(){
+  # 1. anaconda promptで `conda init powershell` を実行
+  # 2. Document/WindowsPowerShell/profile.ps1が生成される
+  # 3. 中身をコピーして以下のように貼り付け、profile.ps1を削除
+  (& "C:\Users\MK\Anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+}
+function cond(){
+  conda deactivate
+}
+function cact(){
+  conda activate $args
+}
+function ccre(){
+  conda create -n $args
+}
+function crem(){
+  conda remove -n $args --all
+}
+function cinf(){
+  conda info -e
+}
